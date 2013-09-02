c187c03d8ba386d4f3823b9802b2cc60f800c930
Matt Stow <matt@mattstow.com>
2013-09-02 16:05:12 +1000

    Initial commit of demo

---
+++ js/mq.genie.js
@@ -0,0 +1,163 @@
+/*!
+* mqGenie v0.4.1
+*
+* Adjusts CSS media queries in browsers that include the scrollbar's width in the viewport width so they fire at the intended size
+*
+* Returns the mqGenie object containing .adjusted, .width & fontSize for use in re-calculating media queries in JavaScript with mqAdjust(string)
+*
+* Copyright (c) 2013 Matt Stow
+*
+* http://mattstow.com
+*
+* Licensed under the MIT license
+*/
+;(function(win, doc) {
+	if (!doc.addEventListener)
+		return;
+	
+	function processRules(stylesheet, processor) {
+		var rules = stylesheet.cssRules ? stylesheet.cssRules : stylesheet.media,
+			rule,
+			processed = [],
+			i = 0,
+			length = rules.length;
+		
+		for (i; i < length; i ++) {
+			rule = rules[i];
+			
+			if (processor(rule))
+				processed.push(rule);
+		}
+		
+		return processed;
+	}
+	
+	function getMediaQueries(stylesheet) {
+		return processRules(stylesheet, function (rule) {
+			return rule.constructor === CSSMediaRule;
+		});
+	}
+	
+	function sameOrigin(url) {
+		var loc = win.location,
+			a = doc.createElement('a');
+		
+		a.href = url;
+		
+		return a.hostname === loc.hostname && a.protocol === loc.protocol;
+	}
+	
+	function isInline(stylesheet) {
+		return stylesheet.ownerNode.constructor === HTMLStyleElement;
+	}
+	
+	function isValidExternal(stylesheet) {
+		return stylesheet.href && sameOrigin(stylesheet.href);
+	}
+	
+	function getStylesheets() {
+		var sheets = doc.styleSheets,
+			sheet,
+			length = sheets.length,
+			i = 0,
+			valid = [];
+		
+		for (i; i < length; i++) {
+			sheet = sheets[i];
+			
+			if (isValidExternal(sheet) || isInline(sheet))
+				valid.push(sheet);
+		}
+		
+		return valid;
+	}
+	
+	doc.addEventListener('DOMContentLoaded', function() {
+		win.mqGenie = (function() {
+			var html = doc.documentElement;
+			
+			html.style.overflowY = 'scroll';
+			
+			var width = win.innerWidth - html.clientWidth,
+				props = {
+					adjusted: width > 0,
+					fontSize: parseFloat(win.getComputedStyle(html).getPropertyValue('font-size')),
+					width: width					
+				};
+			
+			if (props.adjusted) {
+				if ('WebkitAppearance' in html.style) {
+					var chromeRX = /Chrome\/(\d*?\.\d*?\.\d*?\.\d*?)\s/g,
+						chrome = navigator.userAgent.match(chromeRX),
+						chromeVersion;
+					
+					if (chrome) {
+						chrome = chrome[0].replace(chromeRX, '$1');
+						chromeVersion = chrome.split('.');
+						chromeVersion[0] = parseInt(chromeVersion[0]);
+						chromeVersion[2] = parseInt(chromeVersion[2]);
+						chromeVersion[3] = parseInt(chromeVersion[3]);
+						
+						if (chromeVersion[0] <= 29) {
+							if (chromeVersion[0] === 29 && chromeVersion[2] < 1548 && chromeVersion[3] < 57) {
+								props.adjusted = false;
+							}
+							else if (chromeVersion[0] < 29) {
+								props.adjusted = false;
+							}
+						}
+					}
+					else {
+						props.adjusted = false;
+					}
+					
+					if (!props.adjusted)
+						return props;
+				}
+				
+				var stylesheets = getStylesheets(),
+					stylesheetsLength = stylesheets.length,
+					i = 0,
+					mediaQueries,
+					mediaQueriesLength;
+				
+				for (i; i < stylesheetsLength; i++) {
+					mediaQueries = getMediaQueries(stylesheets[i]);
+					mediaQueriesLength = mediaQueries.length;
+					
+					for (var j = 0; j < mediaQueriesLength; j++) {
+						mediaQueries[j].media.mediaText = mediaQueries[j].media.mediaText.replace(/m(in|ax)-width:\s*(\d|\.)+(px|em)/gi, function(strA) {
+							if (strA.match('px')) {
+								return strA.replace(/\d+px/gi, function(strB) {
+									return parseInt(strB, 10) + props.width + 'px';
+								});
+							}
+							else {
+								return strA.replace(/\d.+?em/gi, function(strB) {
+									return ((parseFloat(strB) * props.fontSize) + props.width) / props.fontSize + 'em';
+								});
+							}
+						});
+					}
+				}
+			}
+			
+			return props;
+		})();
+		
+		win.mqAdjust = function(mediaQuery) {
+			if (!mqGenie.adjusted)
+				return mediaQuery;
+			
+			var mq = mediaQuery.replace(/\d+px/gi, function(c) {
+				return parseInt(c, 10) + mqGenie.width + 'px';
+			});
+			
+			mq = mq.replace(/\d.+?em/gi, function(c) {
+				return ((parseFloat(c) * mqGenie.fontSize) + mqGenie.width) / mqGenie.fontSize + 'em';
+			});
+			
+			return mq;
+		};
+	});
+})(window, document);
